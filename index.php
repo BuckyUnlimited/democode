7e2b43115945935553778078f922bf76c46dc670
BuckyUnlimited <--global>
2026-01-09 22:34:01 +0700

    Update code

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 include './includes/header.inc.php';
 include './includes/navbar.inc.php';
 ?>
+    <h1 class="mx-auto p-2 container text-center">
+        Welcome to Our Website
+    </h1>
+
+    <p class="mx-auto p-2 container text-center" style="width: 600px;">
+        This is the home page. Please use the navigation bar to register or login.
+    </p>
 <?php
 include './includes/footer.inc.php';
 ?>
