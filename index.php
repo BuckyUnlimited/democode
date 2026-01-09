1ab68b04d37d71b6753531dd4cdb47e20908ce64
BuckyUnlimited <--global>
2026-01-09 16:33:17 +0700

    Update layout and auth pages

--- index.php
+++ index.php
@@ -1,38 +1,7 @@
 <?php
-require_once './init/db.init.php';
 include './includes/header.inc.php';
 include './includes/navbar.inc.php';
-
-
-$avialable_pages = ['login', 'register'];
-//isset it is used to check whether a variable is set or not
-if (isset($_GET["page"])) {
-    $page = $_GET["page"];
-    if (in_array($page, $avialable_pages)) {
-        include './pages/' . $page . '.php';
-    } else {
-        // echo '<h1>Page Not found</h1>';
-        include './pages/404error.php';
-    }
-} else {
-    include './pages/dashboard.php';
-}
 ?>
-
 <?php
 include './includes/footer.inc.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-
-<body>
-</body>
-
-</html>
--- pages/404error.php
+++
@@ -1,4 +0,0 @@
-<h1>
-    404 Error - Page Not Found
-    <a href="/website.">Go to dashboard</a>
-</h1>
