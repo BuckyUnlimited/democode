0b1cd49ccf6327ebd0165bee6464980f5dbfc5a0
BuckyUnlimited <--global>
2026-01-09 22:59:21 +0700

    Change for v1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include './includes/header.inc.php';
 include './includes/navbar.inc.php';
 ?>
     <h1 class="mx-auto p-2 container text-center">
-        Welcome to Our Website
+        Welcome to Our Website Version main
     </h1>
 
     <p class="mx-auto p-2 container text-center" style="width: 600px;">
